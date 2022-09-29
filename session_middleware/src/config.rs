14abbac86ee77bb2b15c7df76f6b56596dfc48b5
yez <fzy09@outlook.com>
2022-09-29 15:44:52 +0800

    temp commit

--- session_middleware/src/config.rs
+++ session_middleware/src/config.rs
@@ -1,16 +1,57 @@
-use chrono::Duration;
+use std::borrow::Cow;
 
-#[derive(Clone,Debug)]
+use chrono::Duration;
+use cookie::{SameSite, Key};
+
+#[derive(Clone,)] //Debug
 pub struct AxumSessionConfig{
-    pub idle_timeout:Duration,
-    pub memory_clear_timeout:Duration, 
+    pub(crate) idle_timeout:Duration,
+    pub(crate) memory_clear_timeout:Duration, 
+
+    /// Session cookie domain
+    pub(crate) cookie_domain: Option<Cow<'static, str>>,
+    /// Session cookie http only flag
+    pub(crate) cookie_http_only: bool,
+    /// Session cookie path
+    pub(crate) cookie_path: Cow<'static, str>,
+    /// Resticts how Cookies are sent cross-site. Default is `SameSite::Lax`
+    pub(crate) cookie_same_site: SameSite,
+    /// Session cookie secure flag
+    pub(crate) cookie_secure: bool,
+
+    ///Encyption Key used to encypt cookies for confidentiality, integrity, and authenticity.
+    pub(crate) key: Option<Key>,
 }
+
+impl std::fmt::Debug for AxumSessionConfig {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("AxumSessionConfig")
+            .field("idle_timeout", &self.idle_timeout)
+            .field("memory_clear_timeout", &self.memory_clear_timeout)
+            .field("cookie_domain", &self.cookie_domain)
+            .field("cookie_http_only", &self.cookie_http_only)
+            .field("cookie_path", &self.cookie_path)
+            .field("cookie_same_site", &self.cookie_same_site)
+            .field("cookie_secure", &self.cookie_secure)
+            .field("key", &"key hidden")
+            .finish()
+    }
+}
+
 
 impl Default for AxumSessionConfig{
     fn default() -> Self{
         AxumSessionConfig{
             idle_timeout:Duration::days(300),
             memory_clear_timeout:Duration::minutes(10),
+
+            cookie_path: "/".into(),
+            cookie_http_only: true,
+            cookie_secure: false,
+            cookie_domain: None,
+            cookie_same_site: SameSite::Lax,
+
+            key:None,
         }
     }
 }
--- session_middleware/src/service.rs
+++ session_middleware/src/service.rs
@@ -4,9 +4,10 @@ use axum_core::{
     BoxError,
 };
 use bytes::Bytes;
+use cookie::{CookieJar, Cookie, Key};
 use futures::future::BoxFuture;
 use http::{
-    self,Request, HeaderValue,
+    self,Request, HeaderValue, header, HeaderMap,
 };
 use http_body::Body as HttpBody;
 use std::{
@@ -17,9 +18,9 @@ use std::{
     task::{Context, Poll}, sync::{Mutex, Arc},
 };
 use tower_service::Service;
-use chrono::{Local};
+use chrono::{Local, Duration};
 
-use crate::{database_pool::AxumDatabasePool, session_store::AxumSessionStore, session::AxumSession, constants::SESSIONID};
+use crate::{database_pool::AxumDatabasePool, session_store::AxumSessionStore, session::AxumSession, constants::SESSIONID, config::AxumSessionConfig};
 
 #[derive(Clone)]
 pub struct AxumSessionService<S, T>
@@ -57,9 +58,8 @@ where
         let mut ready_inner = std::mem::replace(&mut self.inner, not_ready_inner);
 
         Box::pin(async move {
-            let session_id=req.headers().get(SESSIONID).and_then(|id|id.to_str().ok());
-           
-            let session = AxumSession::load_or_init(&store, session_id).await;
+            let session_id= get_metadata(&req,SESSIONID,store.config.clone());
+            let session = AxumSession::load_or_init(&store, session_id.as_deref()).await;
             store.memory_store.retain(|_k, v|  v.expiry_time>Local::now());
 
             let session=Arc::new(Mutex::new(session)); // 在res返回中取不到同一个Extension
@@ -75,7 +75,7 @@ where
                 tracing::error!("session commit error: {}",e);
             });
 
-            res.headers_mut().insert(SESSIONID, HeaderValue::from_str(session.session_id.0.as_str()).unwrap());
+            update_metadata(&mut res,SESSIONID,session.session_id.0.as_str(),store.config.clone());
 
             Ok(res)
         })
@@ -92,5 +92,103 @@ where
             .field("session_store", &self.session_store)
             .field("inner", &self.inner)
             .finish()
+    }
+}
+
+fn get_metadata<ReqBody>(req:&Request<ReqBody>,name:&str,config:AxumSessionConfig) ->Option<String>{
+    let mut value=req.headers().get(name)
+        .and_then(|id|id.to_str().ok())
+        .map(|x|x.to_string());
+
+    if value.is_none(){
+        let cookies=get_cookies(req);
+
+        value=cookies.get_cookie(name, &config.key)
+        .map(|c| c.value().to_string());
+    }
+
+    value
+}
+
+fn update_metadata(res:&mut Response<BoxBody>,name:&'static str,value:&str,config:AxumSessionConfig){
+    res.headers_mut().insert(name, HeaderValue::from_str(value).unwrap()).unwrap();
+
+    let mut cookies = CookieJar::new();
+
+    cookies.add_cookie(
+        create_cookie(config.clone(), name.to_string(), value.to_string()),
+        &config.key,
+    );
+
+    set_cookies(cookies, res.headers_mut());
+}
+
+pub(crate) trait CookiesExt {
+    fn get_cookie(&self, name: &str, key: &Option<Key>) -> Option<Cookie<'static>>;
+    fn add_cookie(&mut self, cookie: Cookie<'static>, key: &Option<Key>);
+}
+
+impl CookiesExt for CookieJar {
+    fn get_cookie(&self, name: &str, key: &Option<Key>) -> Option<Cookie<'static>> {
+        if let Some(key) = key {
+            self.private(key).get(name)
+        } else {
+            self.get(name).cloned()
+        }
+    }
+
+    fn add_cookie(&mut self, cookie: Cookie<'static>, key: &Option<Key>) {
+        if let Some(key) = key {
+            self.private_mut(key).add(cookie)
+        } else {
+            self.add(cookie)
+        }
+    }
+}
+
+fn create_cookie<'a>(
+    config: AxumSessionConfig,
+    name:String,
+    value: String,
+) -> Cookie<'a> {
+    let mut cookie_builder = Cookie::build(name, value)
+        .path(config.cookie_path.to_owned())
+        .secure(config.cookie_secure)
+        .http_only(config.cookie_http_only)
+        .same_site(config.cookie_same_site);
+
+    if let Some(domain) = &config.cookie_domain {
+        cookie_builder = cookie_builder.domain(domain.to_owned());
+    }
+
+    let time_duration = Duration::max_value().to_std().expect("Max Age out of bounds");
+    cookie_builder = cookie_builder.max_age(time_duration.try_into().expect("Max Age out of bounds"));
+
+    cookie_builder.finish()
+}
+
+fn get_cookies<ReqBody>(req: &Request<ReqBody>) -> CookieJar {
+    let mut jar = CookieJar::new();
+
+    let cookie_iter = req
+        .headers()
+        .get_all(header::COOKIE)
+        .into_iter()
+        .filter_map(|value| value.to_str().ok())
+        .flat_map(|value| value.split(';'))
+        .filter_map(|cookie| Cookie::parse_encoded(cookie.to_owned()).ok());
+
+    for cookie in cookie_iter {
+        jar.add_original(cookie);
+    }
+
+    jar
+}
+
+fn set_cookies(jar: CookieJar, headers: &mut HeaderMap) {
+    for cookie in jar.delta() {
+        if let Ok(header_value) = cookie.encoded().to_string().parse() {
+            headers.append(header::SET_COOKIE, header_value);
+        }
     }
 }
